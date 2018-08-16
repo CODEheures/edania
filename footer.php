ee412e440ac68708c909ee6f4b9b0063d949a4b2
CODEheures <webmaster@codeheures.fr>
2018-08-16 17:22:09 +0200

    change directory struct

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div id="copyright" class="container">
+    <p>&copy; ecortex.fr. All rights reserved. | <a href="./mentions.php">Mentions légales</a> | Design in part by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
